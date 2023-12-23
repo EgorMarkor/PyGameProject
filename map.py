c77b6a308916db33ae89b2ddd6a193fac11e69dd
Ева <zykova.ev06@gmail.com>
2023-12-23 22:50:20 +0400

    level_map

---
+++ map.py
@@ -0,0 +1,9 @@
+def load_level(filename):
+    filename = "data/" + filename
+    with open(filename, 'r') as mapFile:
+        f = [line.strip() for line in mapFile]
+    board=[]
+    for i in range(len(f)):
+        s = [j for j in f[i]]
+        board.append(s)
+    return board
